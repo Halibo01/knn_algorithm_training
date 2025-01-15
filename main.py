7a9df3a6fd8c2afc47de9c7be651891c65da1a3e
Halil İbrahim Sandıraz <halilsan1994@gmail.com>
2025-01-15 15:54:30 +0300

    model not included

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
 import sqlite3
+import os
 
 def convert(liste:torch.Tensor|str, tokenizer = None):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -20,69 +21,71 @@ def convert(liste:torch.Tensor|str, tokenizer = None):
     newtensor = torch.tensor([z_min, z_mean, z_max], dtype=torch.float64).to(device)
     return newtensor
 
-conn = sqlite3.connect('list_data.db')
-cursor = conn.cursor()
+def save_to_db(liste:list, dbname:str = "list_data.db", table_name:str = "qa_table"):
+    with sqlite3.connect(dbname) as conn:
+        cursor = conn.cursor()
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS qa_table (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            question TEXT NOT NULL,
+            answer TEXT NOT NULL,
+            question_z_min FLOAT NOT NULL,
+            question_z_mean FLOAT NOT NULL,
+            question_z_max FLOAT NOT NULL,
+            answer_z_min FLOAT NOT NULL,
+            answer_z_mean FLOAT NOT NULL,
+            answer_z_max FLOAT NOT NULL
+        );
+        """)
 
-model_name = r"C:\Users\Asus\Documents\github\Qwen2.5-Coder-1.5B-Instruct-GPTQ-Int4"
-
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS qa_table (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    question TEXT NOT NULL,
-    answer TEXT NOT NULL,
-    question_z_min FLOAT NOT NULL,
-    question_z_mean FLOAT NOT NULL,
-    question_z_max FLOAT NOT NULL,
-    answer_z_min FLOAT NOT NULL,
-    answer_z_mean FLOAT NOT NULL,
-    answer_z_max FLOAT NOT NULL
-);
-""")
-
-
-# user: llgdfgdfgldjfdjgdfg
-# asistan:
-
-tokenizer = AutoTokenizer.from_pretrained(r"C:\Users\Asus\Desktop\mysql_example\tokens\qwen")
-model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype="auto", device_map="cuda")
-
-question = "Merhaba! Benim adım Halil İbrahim."
+        cursor.execute("""
+        INSERT INTO qa_table (question, answer, question_z_min, question_z_mean, question_z_max, answer_z_min, answer_z_mean, answer_z_max)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+        """, liste
+        )
 
 
+def main():
+
+    model_name = r"tokens\qwen"
+    model_name = os.path.abspath(model_name)
+
+    
+
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    # model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype="auto", device_map="cuda")
+
+    question = "Merhaba! Benim adım Halil İbrahim."
 
 
 
-messages = [
-    {"role": "system", "content": "Karşındaki kişiye arkadaşlık eden bir arkadaşsın."},
-    {"role": "system", "content": "Adın Cuma."},
-    {"role": "user", "content": question},
-]
-
-text = tokenizer.apply_chat_template(
-    messages,
-    tokenize=False,
-    add_generation_prompt=True
-)
-
-model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
-
-print(convert(messages[-1]["content"], tokenizer=tokenizer))
-# print(convert(question))
-
-generated_ids = model.generate(
-    **model_inputs,
-    max_new_tokens=512
-)
-
-generated_ids = [
-    output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
-]
 
 
-print(convert(generated_ids[0]))
+    messages = [
+        {"role": "system", "content": "Karşındaki kişiye arkadaşlık eden bir arkadaşsın."},
+        {"role": "system", "content": "Adın Cuma."},
+        {"role": "user", "content": question},
+    ]
 
-response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
-print(response)
+    text = tokenizer.apply_chat_template(
+        messages,
+        tokenize=False,
+        add_generation_prompt=True
+    )
+
+    model_inputs = tokenizer([text], return_tensors="pt")
+
+    print(convert(messages[-1]["content"], tokenizer=tokenizer))
+    # print(convert(question))
+
+    # generated_ids = model.generate(**model_inputs, max_new_tokens=512)
+
+    # generated_ids = [output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)] # çıtkı tensor
+
+
+    # print(convert(generated_ids[0]))
+
+    # response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
 
 
 
